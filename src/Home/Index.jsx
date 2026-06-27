3d87a963561fb01e2ca7b0a1393206c0f6fab561
Rafael de Moura Cassiano Silva <rafael.silva.700@ufrn.edu.br>
2026-06-27 10:06:06 -0300

    mapa-base

--- src/Home/Index.jsx
+++ src/Home/Index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './Style.css'
 import Navbar from './Navbar'
 import Hero from './Hero'
+import Mapa from '../Mapa/Index'
 
 function Index({ onNavigate }) {
   return (
     <div className="home-page">
       <Navbar onNavigate={onNavigate} />
       <Hero onNavigate={onNavigate} />
+      <Mapa />
     </div>
   )
 }
